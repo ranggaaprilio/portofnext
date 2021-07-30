5052e075f5d8e6137683534c594364ba6d6f4778
Rangga Aprilio Utama <Ranggaaprillio@gmail.com>
2021-07-30 16:36:48 +0700

    update portofolio

--- components/template/portofolio/section1.jsx
+++ components/template/portofolio/section1.jsx
@@ -16,13 +16,13 @@ export const Section1 = () => {
                 <div className=" mx-auto">
                     <div className="relative z-10 ">
                             <Navbar />
-                        <div className="md:flex  md:mt-6 md:mb-3 h-48 w-screen bg-indigo-700">
-
+                        <div className="flex  md:mt-6 md:mb-3 h-48 w-screen bg-indigo-700 items-center ">
+                            <h1 className="md:text-4xl sm:text-sm font-size-1 mt-2 ml-10 text-white">Life Hasn't CTRL+Z</h1>
                         </div>
                         <div className="mx-8 md:container">
                             <div className="md:flex justify-between mt-5 mb-5">
                                 <div className=" sm:mx-2 md:mr-10 w-auto h-3/4">
-                                <img src="/images/porto/niu.png" alt="" srcset="" />
+                                    <img src="/images/porto/niu.png" alt="" srcset="" className="rounded-xl"/>
                                 </div>
                                 <div className=" w-auto">
                                     <h1 className="text-lg font-bold mt-8">NIU Digital CRM</h1>
@@ -31,7 +31,7 @@ export const Section1 = () => {
                             </div>
                             <div className="md:flex justify-between mt-5 mb-5"> 
                                 <div className="  w-auto h-3/4  md:order-2 ">
-                                    <img src="/images/porto/devcon.png" alt="" srcset="" />
+                                    <img src="/images/porto/devcon.png" alt="" srcset="" className="rounded-xl" />
                                 </div>
                                 <div className="sm:mx-2 w-auto  md:order-1  ">
                                     <h1 className="text-lg font-bold mt-8">Konekin Dev</h1>
@@ -40,8 +40,8 @@ export const Section1 = () => {
                                 
                             </div>
                             <div className="md:flex justify-between mt-5 mb-5">
-                                <div className=" sm:mx-2 md:mr-10 w-auto h-3/4">
-                                <img src="/images/porto/Bikincv.png" alt="" srcset="" />
+                                <div className=" sm:mx-2 md:mr-10 w-auto h-3/4 ">
+                                    <img src="/images/porto/Bikincv.png" alt="" srcset="" className="rounded-xl" />
                                 </div>
                                 <div className=" w-auto">
                                     <h1 className="text-lg font-bold mt-8">BikinCVmu!</h1>
@@ -50,7 +50,7 @@ export const Section1 = () => {
                             </div>
                             <div className="md:flex justify-between mt-5 mb-5"> 
                                 <div className="  w-auto h-3/4  md:order-2 ">
-                                    <img src="/images/porto/Quran.png" alt="" srcset="" />
+                                    <img src="/images/porto/Quran.png" alt="" srcset="" className="rounded-xl"/>
                                 </div>
                                 <div className="sm:mx-2 w-auto  md:order-1  ">
                                     <h1 className="text-lg font-bold mt-8">Qur'an App</h1>
@@ -60,7 +60,7 @@ export const Section1 = () => {
                             </div>
                             <div className="md:flex justify-between mt-5 mb-5">
                                 <div className=" sm:mx-2 md:mr-10 w-auto h-3/4">
-                                <img src="/images/porto/BNI.png" alt="" srcset="" />
+                                    <img src="/images/porto/BNI.png" alt="" srcset="" className="rounded-xl"/>
                                 </div>
                                 <div className=" w-auto">
                                     <h1 className="text-lg font-bold mt-8">BNI Telemarketing</h1>
@@ -69,7 +69,7 @@ export const Section1 = () => {
                             </div>
                             <div className="md:flex justify-between mt-5 mb-5"> 
                                 <div className=" w-auto h-3/4  md:order-2 ">
-                                    <img src="/images/porto/cdd.PNG" alt="" srcset="" />
+                                    <img src="/images/porto/cdd.PNG" alt="" srcset="" className="rounded-xl"/>
                                 </div>
                                 <div className="sm:mx-2 w-auto  md:order-1  ">
                                     <h1 className="text-lg font-bold mt-8">HSBC Telesales</h1>
@@ -79,7 +79,7 @@ export const Section1 = () => {
                             </div>
                             <div className="md:flex justify-between mt-5 mb-5">
                                 <div className=" sm:mx-2 md:mr-10 w-auto h-3/4">
-                                <img src="/images/porto/carrer.png" alt="" srcset="" />
+                                    <img src="/images/porto/carrer.png" alt="" srcset="" className="rounded-xl"/>
                                 </div>
                                 <div className=" w-auto">
                                     <h1 className="text-lg font-bold mt-8">Carrer Aseanindo</h1>
